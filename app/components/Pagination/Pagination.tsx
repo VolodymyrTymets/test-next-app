7bca1ec9da6a628daca73c826cefe32fa7546347
Volodymyr Tymets <volodymyrtymets@gmail.com>
2024-06-12 16:08:57 +0300

    Pagination v1 (#6)
    * refactor pagination

--- app/components/Pagination/Pagination.tsx
+++ app/components/Pagination/Pagination.tsx
@@ -3,6 +3,16 @@ import classNames from 'classnames';
 
 import ArrowLeftIcon from '@/app/components/icons/ArrowLeftIcon/ArrowLeftIcon';
 import ArrowRightIcon from '@/app/components/icons/ArrowRightIcon/ArrowRightIcon';
+
+const MAX_PAGES = 6;
+const arrowButtonCass =
+  'flex items-center justify-center px-4 py-2 mx-1 text-gray-500 capitalize bg-white rounded-md  rtl:-scale-x-100 dark:bg-gray-800 dark:text-gray-600';
+const numberButtonClass =
+  'hidden flex items-center justify-center px-4 py-2 mx-1 text-gray-500 capitalize bg-white rounded-md md:inline rtl:-scale-x-100 dark:bg-gray-800 dark:text-gray-600';
+const activeClass =
+  'hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200';
+const inActiveClass = 'cursor-not-allowed';
+
 export default function Pagination({
   path,
   paginationOptions,
@@ -16,17 +26,27 @@ export default function Pagination({
 }) {
   const { totalCount, perPage, currentPage } = paginationOptions;
   const pages = Math.ceil(totalCount / perPage);
-  const active =
-    'hover:bg-blue-500 dark:hover:bg-blue-500 hover:text-white dark:hover:text-gray-200';
-  const inActive = 'cursor-not-allowed';
+
+  const all = Array.from({ length: pages })
+    .map((_, i) => i)
+    .slice(
+      pages - currentPage >= MAX_PAGES ? currentPage - 1 : pages - MAX_PAGES,
+      pages
+    );
+  const head = all.length > MAX_PAGES ? all.slice(0, MAX_PAGES / 2) : all;
+  const tail =
+    all.length > MAX_PAGES
+      ? all.slice(all.length - MAX_PAGES / 2, all.length)
+      : [];
 
   return (
     <div className="flex">
       <a
         href={`${path}?page=${currentPage - 1}`}
         className={classNames([
-          'inline flex items-center justify-center px-4 py-2 mx-1 text-gray-500 capitalize bg-white rounded-md md:hidden rtl:-scale-x-100 dark:bg-gray-800 dark:text-gray-600',
-          currentPage === 1 ? inActive : active,
+          'hidden md:inline',
+          arrowButtonCass,
+          currentPage === 1 ? inActiveClass : activeClass,
         ])}
       >
         <ArrowLeftIcon className="w-5 h-5" />
@@ -34,20 +54,43 @@ export default function Pagination({
       <a
         href={path}
         className={classNames([
-          'hidden flex items-center justify-center px-4 py-2 mx-1 text-gray-500 capitalize bg-white rounded-md md:inline rtl:-scale-x-100 dark:bg-gray-800 dark:text-gray-600',
-          currentPage === 1 ? inActive : active,
+          'inline md:hidden',
+          arrowButtonCass,
+          currentPage === 1 ? inActiveClass : activeClass,
         ])}
       >
         <ArrowLeftIcon className="w-5 h-5" />
       </a>
 
-      {Array.from({ length: pages }).map((_, index) => (
+      {head.map(index => (
         <Link
           key={`${path}-${index}`}
           href={`${path}?page=${index + 1}`}
           className={classNames([
-            'hidden px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-white rounded-md md:inline dark:bg-gray-800 dark:text-gray-200',
-            currentPage === index + 1 ? inActive : active,
+            numberButtonClass,
+            currentPage === index + 1 ? inActiveClass : activeClass,
+          ])}
+        >
+          {index + 1}
+        </Link>
+      ))}
+
+      {tail.length ? (
+        <Link
+          href={`#`}
+          className={classNames([numberButtonClass, inActiveClass])}
+        >
+          ...
+        </Link>
+      ) : null}
+
+      {tail.map(index => (
+        <Link
+          key={`${path}-${index}`}
+          href={`${path}?page=${index + 1}`}
+          className={classNames([
+            numberButtonClass,
+            currentPage === index + 1 ? inActiveClass : activeClass,
           ])}
         >
           {index + 1}
@@ -57,8 +100,9 @@ export default function Pagination({
       <a
         href={`${path}?page=${pages}`}
         className={classNames([
-          'hidden flex items-center justify-center px-4 py-2 mx-1 text-gray-500 capitalize bg-white rounded-md md:inline rtl:-scale-x-100 dark:bg-gray-800 dark:text-gray-600',
-          currentPage === pages ? inActive : active,
+          'hidden md:inline',
+          arrowButtonCass,
+          currentPage === pages ? inActiveClass : activeClass,
         ])}
       >
         <ArrowRightIcon className="w-5 h-5" />
@@ -67,8 +111,9 @@ export default function Pagination({
       <a
         href={`${path}?page=${currentPage + 1}`}
         className={classNames([
-          'inline flex items-center justify-center px-4 py-2 mx-1 text-gray-500 capitalize bg-white rounded-md md:hidden rtl:-scale-x-100 dark:bg-gray-800 dark:text-gray-600',
-          currentPage === pages ? inActive : active,
+          'inline md:hidden',
+          arrowButtonCass,
+          currentPage === pages ? inActiveClass : activeClass,
         ])}
       >
         <ArrowRightIcon className="w-5 h-5" />
